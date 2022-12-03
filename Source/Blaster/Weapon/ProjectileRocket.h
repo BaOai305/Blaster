2199ea0c8caf2228093690d42d431ae49f7fafce
BaOai305 <orehx01@gmail.com>
2022-12-03 23:07:29 +0700

    spawning rocket trail

--- Source/Blaster/Weapon/ProjectileRocket.h
+++ Source/Blaster/Weapon/ProjectileRocket.h
@@ -15,9 +15,40 @@ class BLASTER_API AProjectileRocket : public AProjectile
 	GENERATED_BODY()
 public:
 	AProjectileRocket();
+	virtual void Destroyed() override;
+	
 protected:
 	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) override;
+	
+	virtual void BeginPlay() override;
+	void DestroyTimerFinished();
+
+	// Components
+	UPROPERTY(EditAnywhere)
+		class UNiagaraSystem* TrailSystem;
+
+	// Rocket Mesh
+	UPROPERTY()
+		class UNiagaraComponent* TrailSystemComponent;
+
+	// Sound
+	UPROPERTY(EditAnywhere)
+		USoundCue* ProjectileLoop;
+
+	UPROPERTY()
+		UAudioComponent* ProjectileLoopComponent;
+
+	UPROPERTY(EditAnywhere)
+		USoundAttenuation* LoopingSoundAttenuation;
+
 private:
+	// Rocket Mesh
 	UPROPERTY(VisibleAnywhere)
 		UStaticMeshComponent* RocketMesh;
+
+	FTimerHandle DestroyTimer;
+
+	UPROPERTY(EditAnywhere)
+		float DestroyTime = 1.5f;
+	
 };
